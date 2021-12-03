60f4287aa3c802ea5f574075c20ed378c80bd4c4
Joel Guerra Flores <joel.guerra.contractor@bbva.com>
2021-12-03 16:19:53 -0600

    Primer commit
    Proyecto creado

---
+++ Prueba NACH/Network/Network.swift
@@ -0,0 +1,35 @@
+//
+//  Network.swift
+//  Prueba NACH
+//
+//  Created by Joel Guerra on 03/12/21.
+//
+
+import Foundation
+
+class Network {
+    
+    static let shared = Network()
+    
+    func fetchGenericJSONData<T: Decodable>(urlString: String, completion: @escaping (T?, Error?) -> ()){
+        guard let url = URL(string: urlString) else {return}
+        URLSession.shared.dataTask(with: url) { (data, urlResponse, error) in
+            if let error = error{
+                print("Error fetching ", error)
+                completion(nil, error)
+                return
+            }
+            
+            guard let data = data else {return}
+            print(data)
+            do{
+                let objects = try JSONDecoder().decode(T.self, from: data)
+                print(objects)
+                completion(objects,nil)
+            } catch let jsonError{
+                print("Error decode json ", jsonError)
+                completion(nil, jsonError)
+            }
+        }.resume()
+    }
+}
